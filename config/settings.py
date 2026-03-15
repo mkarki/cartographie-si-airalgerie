cda5ff3283da8fd05d6e4d6c6a394a4380fad5e6
mkarki <ton-email-mkarki@example.com>
2026-03-15 14:52:05 +0100

    fix: STATIC_URL with leading slash + use CompressedStaticFilesStorage for reliable logo serving

--- config/settings.py
+++ config/settings.py
@@ -84,9 +84,9 @@ TIME_ZONE = 'Africa/Algiers'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
